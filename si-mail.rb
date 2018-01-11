a675d8650ec7f6d3eb5824ac1ea18cbed343493a
selesdepselesnul <moch.deden.r@gmail.com>
2018-01-11 09:34:27 +0700

    add exceptionc

--- si-mail.rb
+++ si-mail.rb
@@ -5,21 +5,28 @@ if ARGV.count == 0
   puts "fill email"
 else
 
-  cli = HighLine.new
-  password = cli.ask("Enter your password:  ") { |q| q.echo = "x" }
-  email = ARGV[0]
-  opt = ARGV[1]
+  begin
+    
+      cli = HighLine.new
+      password = cli.ask("Enter your password:  ") { |q| q.echo = "x" }
+      email = ARGV[0]
+      opt = ARGV[1]
 
-  if opt == "--delete-all"
-    gmail = Gmail.new(email, password)
+      if opt == "--delete-all"
+        gmail = Gmail.new(email, password)
 
-    gmail.inbox.emails(:all).each do |x|
-      puts "remove message -> #" + x.uid.to_s
-      x.delete!
-    end
-    gmail.logout
-  else
-    puts "option doesnt valid!"
+        gmail.inbox.emails(:all).each do |x|
+          puts "remove message -> #" + x.uid.to_s
+          x.delete!
+        end
+        gmail.logout
+      else
+        puts "option doesnt valid!"
+      end
+
+  rescue Exception => e
+    puts "Error running script: " + e.message
   end
   
 end
+
