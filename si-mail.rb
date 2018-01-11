3b69eca098fd6ad0994e305e55fa58ab7b0c885f
selesdepselesnul <moch.deden.r@gmail.com>
2018-01-11 10:05:36 +0700

    refactor to function

--- si-mail.rb
+++ si-mail.rb
@@ -1,44 +1,43 @@
 require 'gmail'
 require 'highline'
 
+def delete_message(email, password, flag)
+  gmail = Gmail.new(email, password)
+  emails = gmail.inbox.emails(flag)
+
+  if emails.length == 0
+    puts "nothing to be delete"
+  else
+    emails.each do |x|
+      puts "remove message -> " + x.inspect.to_s
+      x.delete!
+    end
+  end
+  
+  gmail.logout
+end
+
 if ARGV.count == 0
   puts "fill email"
 else
-
-  begin
-    
+  begin    
       cli = HighLine.new
-      password = cli.ask("Enter your password:  ") { |q| q.echo = "*" }
+
       email = ARGV[0]
+      password = cli.ask("Enter your password:  ") { |q| q.echo = "*" }
+
       opt = ARGV[1]
 
       if opt == "--delete-all"
-        gmail = Gmail.new(email, password)
-        all_email = gmail.inbox.emails(:all)
-
-        if all_email.length == 0
-          puts "nothing to be delete"
-        else
-          all_email.each do |x|
-            puts "remove message -> " + x.inspect.to_s
-            x.delete!
-          end
-        end
-        
-        gmail.logout
+        delete_message(email, password, :all)
       elsif opt == "--delete-read"
-        gmail = Gmail.new(email, password)
-        unread_email = gmail.inbox.emails(:read)
-        unread_email.each do |x|
-          puts "remove message -> " + x.inspect.to_s
-          x.delete!
-        end
+        delete_message(email, password, :read)
       else
         puts "option doesnt valid!"
       end
   rescue Exception => e
     puts "Error running script: " + e.message
-  end
-  
+  end  
 end
+
 
