058ed1f8eda72d225da2cea0b6a52b05f78576de
selesdepselesnul <moch.deden.r@gmail.com>
2018-01-12 10:04:22 +0700

    refactor to class

--- si-mail.rb
+++ si-mail.rb
@@ -1,21 +1,31 @@
 require 'gmail'
 require 'highline'
 
-def delete_message(email, password, flag)
-  gmail = Gmail.new(email, password)
-  emails = gmail.inbox.emails(flag)
-
-  if emails.length == 0
-    puts "nothing to be delete"
-  else
-    emails.each do |x|
-      puts "remove message -> " + x.inspect.to_s
-      x.delete!
-    end
-  end
+class SiMail
   
-  gmail.logout
+   def initialize(email, password)
+    @email = email
+    @password = password
+   end
+
+   def delete!(flag)
+     gmail = Gmail.new(@email, @password)
+     emails = gmail.inbox.emails(flag)
+
+     if emails.length == 0
+       puts "nothing to be delete"
+     else
+       emails.each do |x|
+         puts "remove message -> " + x.inspect.to_s
+         x.delete!
+       end
+     end
+  
+     gmail.logout
+   end
+   
 end
+
 
 if ARGV.count == 0
   puts "fill email"
@@ -26,12 +36,13 @@ else
       email = ARGV[0]
       password = cli.ask("Enter your password:  ") { |q| q.echo = "*" }
 
-      opt = ARGV[1]
+      simail = SiMail.new(email, password)
 
+      opt = ARGV[1]
       if opt == "--delete-all"
-        delete_message(email, password, :all)
+        simail.delete!(:all)
       elsif opt == "--delete-read"
-        delete_message(email, password, :read)
+        simail.delete!(:read)
       else
         puts "option doesnt valid!"
       end
